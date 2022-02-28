f319592d5c45d7f3aa1c546e76efb332c710c0af
dankvid <78324552+dankvid@users.noreply.github.com>
2022-02-28 08:51:06 +0100

    Add files via upload
    discord side panel fix

---
+++ fixed_discord.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+exec /usr/bin/discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy
